d146738d8474f6db194b9a112886661543575c4a
Burenkovuser <nb2207@yandex.ru>
2017-06-08 11:47:52 +0700

    Initial Commit

---
+++ Seg Nav/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Seg Nav
+//
+//  Created by Vasilii on 08.06.17.
+//  Copyright © 2017 Vasilii Burenkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Seg Nav/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Seg Nav
+//
+//  Created by Vasilii on 08.06.17.
+//  Copyright © 2017 Vasilii Burenkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
